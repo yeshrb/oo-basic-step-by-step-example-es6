f98effd0a7febe0b92c7ba89d8e993388a5fa4b3
ljj <yeshrb@126.com>
2018-05-10 06:52:20 +0800

    complete practice_2

--- src/practice_2/student.js
+++ src/practice_2/student.js
@@ -1,1 +1,11 @@
+import Person from './person';
 
+export default class Student extends Person{
+    constructor(name,age,klass){
+        super(name,age);
+        this.klass = klass;
+    }
+    introduce(){
+        return `I am a Student. I am at Class ${this.klass}.`
+    }
+}
