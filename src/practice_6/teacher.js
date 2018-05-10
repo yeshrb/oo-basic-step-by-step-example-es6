40bc55660a7642c6e0979ee85e502921f26dd81a
ljj <yeshrb@126.com>
2018-05-10 22:06:42 +0800

    complete practice_5

--- src/practice_6/teacher.js
+++ src/practice_6/teacher.js
@@ -1,1 +1,13 @@
+import Person from '../practice_1/person';
 
+export default class Teacher extends Person {
+    constructor(name,age,klass){
+        super(name,age);
+        this.klass = klass;
+    }
+
+    introduce(){
+        let clss = this.klass? `Class ${this.klass}` :`No Class`
+        return super.introduce() + ` I am a Teacher. I teach ${clss}.`;
+    }
+};
