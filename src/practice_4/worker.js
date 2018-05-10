0f2ea1dc6ad292498a4085b6e4526e8aaaf70269
ljj <yeshrb@126.com>
2018-05-10 21:49:24 +0800

    complete practice_4

--- src/practice_4/worker.js
+++ src/practice_4/worker.js
@@ -1,1 +1,9 @@
-
+import Person from './person';
+export default class Worker extends Person{
+    constructor(name,age){
+        super(name,age);
+    }
+    introduce() {
+        return super.introduce() + ` I am a Worker. I have a job.`;
+    }
+}
